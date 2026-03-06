f038d183f55e130695fa20229e1e5d175d8a720b
Sean-tsy <e1451196@u.nus.edu>
2026-03-06 01:24:00 +0000

    modify utils.py

--- st5230-assignment1/src/utils.py
+++ st5230-assignment1/src/utils.py
@@ -210,7 +210,7 @@ def epoch_progress_bar(
     return tqdm(
         dataloader,
         desc=f"{desc} [{epoch}/{total_epochs}]",
-        leave=False,
+        leave=True,
         dynamic_ncols=True,
     )
 
